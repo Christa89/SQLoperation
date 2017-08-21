70a02ec89ab79e6fa9126f9b31fb339bdf978673
christa89 <christa.bsc@gmial.com>
2017-08-21 17:30:53 +0530

    committed

--- SQLOperation/Form1.Designer.cs
+++ SQLOperation/Form1.Designer.cs
@@ -28,12 +28,276 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.createTableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.addPrimaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlTable = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtlength2 = new System.Windows.Forms.TextBox();
+            this.txtlength1 = new System.Windows.Forms.TextBox();
+            this.cmbDatatype = new System.Windows.Forms.ComboBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip2.SuspendLayout();
+            this.pnlTable.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(669, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createTableToolStripMenuItem,
+            this.addToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(669, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // createTableToolStripMenuItem
+            // 
+            this.createTableToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem1,
+            this.addPrimaryToolStripMenuItem});
+            this.createTableToolStripMenuItem.Name = "createTableToolStripMenuItem";
+            this.createTableToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.createTableToolStripMenuItem.Text = "Create Table";
+            // 
+            // addToolStripMenuItem1
+            // 
+            this.addToolStripMenuItem1.Name = "addToolStripMenuItem1";
+            this.addToolStripMenuItem1.Size = new System.Drawing.Size(140, 22);
+            this.addToolStripMenuItem1.Text = "Add";
+            this.addToolStripMenuItem1.Click += new System.EventHandler(this.addToolStripMenuItem1_Click);
+            // 
+            // addPrimaryToolStripMenuItem
+            // 
+            this.addPrimaryToolStripMenuItem.Name = "addPrimaryToolStripMenuItem";
+            this.addPrimaryToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.addPrimaryToolStripMenuItem.Text = "Add Primary";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.addToolStripMenuItem.Text = "Add";
+            // 
+            // pnlTable
+            // 
+            this.pnlTable.Controls.Add(this.panel1);
+            this.pnlTable.Controls.Add(this.checkBox1);
+            this.pnlTable.Controls.Add(this.textBox2);
+            this.pnlTable.Controls.Add(this.label2);
+            this.pnlTable.Controls.Add(this.label1);
+            this.pnlTable.Controls.Add(this.textBox1);
+            this.pnlTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlTable.Location = new System.Drawing.Point(0, 48);
+            this.pnlTable.Name = "pnlTable";
+            this.pnlTable.Size = new System.Drawing.Size(669, 283);
+            this.pnlTable.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtlength2);
+            this.panel1.Controls.Add(this.txtlength1);
+            this.panel1.Controls.Add(this.cmbDatatype);
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(669, 283);
+            this.panel1.TabIndex = 5;
+            // 
+            // txtlength2
+            // 
+            this.txtlength2.Location = new System.Drawing.Point(556, 76);
+            this.txtlength2.Name = "txtlength2";
+            this.txtlength2.Size = new System.Drawing.Size(72, 20);
+            this.txtlength2.TabIndex = 8;
+            // 
+            // txtlength1
+            // 
+            this.txtlength1.Location = new System.Drawing.Point(466, 76);
+            this.txtlength1.Name = "txtlength1";
+            this.txtlength1.Size = new System.Drawing.Size(71, 20);
+            this.txtlength1.TabIndex = 7;
+            // 
+            // cmbDatatype
+            // 
+            this.cmbDatatype.FormattingEnabled = true;
+            this.cmbDatatype.Location = new System.Drawing.Point(466, 52);
+            this.cmbDatatype.Name = "cmbDatatype";
+            this.cmbDatatype.Size = new System.Drawing.Size(162, 21);
+            this.cmbDatatype.TabIndex = 6;
+            this.cmbDatatype.SelectedIndexChanged += new System.EventHandler(this.cmbDatatype_SelectedIndexChanged);
+
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(390, 79);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(61, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "NotNull";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(390, 56);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(60, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Identity";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(139, 53);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(245, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Table Column s";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Table Name ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(139, 13);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(489, 20);
+            this.textBox4.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(324, 56);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Identity";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(139, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(178, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Table Column s";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Table Name ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 331);
+            this.Controls.Add(this.pnlTable);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.pnlTable.ResumeLayout(false);
+            this.pnlTable.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem createTableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem addPrimaryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.Panel pnlTable;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cmbDatatype;
+        private System.Windows.Forms.TextBox txtlength2;
+        private System.Windows.Forms.TextBox txtlength1;
+
     }
 }
 
--- SQLOperation/Form1.cs
+++ SQLOperation/Form1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Configuration;
+using System.Text.RegularExpressions;
 
 namespace SQLOperation
 {
@@ -15,6 +19,76 @@ namespace SQLOperation
         public Form1()
         {
             InitializeComponent();
+            DataTypes();
+            pnlTable.Visible = false;
+
+            txtlength1.Visible = false;
+            txtlength2.Visible = false;
+
+
+            cmbDatatype.ValueMember = "id";
+            cmbDatatype.DisplayMember = "datatype";
+            cmbDatatype.DataSource = DataTypes().Tables[0];
+
+        }
+
+        private void addToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            pnlTable.Visible = true;
+        }
+
+
+        //private void cmbDatatype_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    string typeArray = System.Configuration.ConfigurationManager.AppSettings["chartype1"];
+        //    string[] type = typeArray.Split(',');
+        //    string value = cmbDatatype.SelectedText;
+
+        //    if (type.Contains(value))
+        //  {
+        //      txtlength1.Visible = true;
+        //  }
+        //}
+
+
+        public DataSet DataTypes()
+        {
+            string fileName = System.Configuration.ConfigurationManager.AppSettings["DataTypeXmlFilepth"];
+            XmlReader xmlFile;
+            xmlFile = XmlReader.Create(fileName, new XmlReaderSettings());
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlFile);
+            return ds;
+        }
+
+        private void cmbDatatype_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string typeArray1 = System.Configuration.ConfigurationManager.AppSettings["chartype1"];
+            string typeArray2 = System.Configuration.ConfigurationManager.AppSettings["chartype2"];
+            string[] type1 = typeArray1.Split(',');
+            string[] type2 = typeArray1.Split(',');
+            string value = Regex.Replace(cmbDatatype.SelectedValue.ToString(), @"\t|\n|\r", ""); ;
+          
+
+            if (type1.Contains(value.Trim(' ')))
+            {
+                txtlength1.Visible = true;
+            }
+            else
+            {
+                txtlength1.Visible = false;
+            }
+
+            if (type2.Contains(value.Trim(' ')))
+            {
+                txtlength1.Visible = true;
+                txtlength2.Visible = true;
+            }
+            else
+            {
+                txtlength1.Visible = true;
+                txtlength2.Visible = false;
+            }
         }
     }
 }
